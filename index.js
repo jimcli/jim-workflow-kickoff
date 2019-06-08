26ccff0ac203236bd8a2e04e18972b56ab7c02b8
Philipp Nowinski <philipp@nowinski.de>
2019-06-08 11:51:17 +0200

    show loading spinner and change to the correct directory before running scripts

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ module.exports = class JimKickoff {
 		}
 
 		if (this.jim.params.length > 1) {
-			this.targetDirectory = this.jim.params[1];
+			[, this.targetDirectory] = this.jim.params;
 		} else {
 			this.targetDirectory = '.';
 		}
@@ -36,14 +36,19 @@ module.exports = class JimKickoff {
 	async run() {
 		this.jim.Logger.info(`Kicking off ${this.jim.params[0]}`);
 		this.jim.Logger.info(`Using "${this.config.repo}" as a template`);
+		this.jim.Logger.showLoadingSpinner('Cloning template repo');
 		await exec(`git clone ${this.config.repo} ${this.targetDirectory}`);
-		this.jim.Logger.success('Template cloned succesfully');
+		this.jim.Logger.removeLoadingSpinner();
+		process.chdir(this.targetDirectory);
+		this.jim.Logger.success('Template cloned successfully');
 		rimraf.sync('.git');
 		this.jim.Logger.success('Removed old git');
 		await exec('git init');
 		this.jim.Logger.success('Initialized a new git repo');
-		this.jim.Logger.info('Running init script');
+		this.jim.Logger.showLoadingSpinner('Running init script');
 		const { stdout } = await exec(this.config.init);
+		this.jim.Logger.removeLoadingSpinner();
+		this.jim.Logger.success('Init script ran successfully');
 		this.jim.Logger.info(stdout);
 		this.jim.Logger.success('All done, happy coding 😎');
 	}
